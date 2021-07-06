2b406220093e04c9637cb7cbcbfdd32b91dfb381
Killoran Lehenbauer <k.lehenbauer13@gmail.com>
2021-07-05 20:20:08 -0700

    Updated image url's

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 module.exports = [
     {
         name: "Jane Doe",
-        photo: "https://unsplash.com/photos/mEZ3PoFGs_k",
+        photo: "https://unsplash.com/photos/aoQ4DYZLE_E",
         scores: ["3", "1", "2", "2", "5", "4", "1", "3", "5", "3"]
     },
     {
         name: "John Public",
-        photo: "https://unsplash.com/photos/vMV6r4VRhJ8",
+        photo: "https://unsplash.com/photos/DItYlc26zVI",
         scores: ["4", "2", "3", "5", "1", "2", "3", "1", "1", "5"]
     },
     {
         name: "Anne Igma",
-        photo: "https://unsplash.com/photos/6W4F62sN_yI",
+        photo: "https://unsplash.com/photos/W7b3eDUb_2I",
         scores: ["1", "5", "4", "5", "2", "3", "3", "4", "1", "4"]
     },
 ]
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const htmlRoutes = require("./app/routing/htmlRoutes")
 const apiRoutes = require("./app/routing/apiRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
